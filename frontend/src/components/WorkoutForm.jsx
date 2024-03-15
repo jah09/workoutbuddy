a1cf195f976342bac39693daa9b8bee493d70e72
jah09 <jasuhot1@gmail.com>
2024-03-15 17:45:38 +0800

    working with update but not working atm. V1

--- frontend/src/components/WorkoutForm.jsx
+++ frontend/src/components/WorkoutForm.jsx
@@ -1,16 +1,17 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 const WorkoutForm = ({ onNewWorkout, workout, isEdit }) => {
-  console.log("workoutform line 4", workout);
+  const [formData, setFormData] = useState({ title: "", load: "", reps: "" });
   const [title, setTitle] = useState("");
   const [load, setLoad] = useState("");
   const [reps, setReps] = useState("");
   const [error, setError] = useState(null);
   const [emptyFields, setEmptyFields] = useState([]);
-  const [formData, setFormData] = useState({ title: "", load: "", reps: "" });
+
   useEffect(() => {
     if (isEdit && workout) {
       // If in edit mode and workout data is provided, set the form data to the workout data
+      console.log("the res is 14", workout);
       setFormData(workout);
     } else {
       // If not in edit mode, clear the form data
@@ -21,34 +22,57 @@ const WorkoutForm = ({ onNewWorkout, workout, isEdit }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const workout = { title, load, reps };
-    try {
-      const response = await axios.post("/api/workouts", workout);
-      console.log("the res is ", response);
-
-      if (response.status === 200) {
-        onNewWorkout(response.data);
-        setTitle("");
-        setLoad("");
-        setReps("");
-        setEmptyFields([]);
-        setError();
+    //const workout = { title, load, reps };
+    if (isEdit) {
+      console.error("Erro27r:", formData);
+      try {
+        const response = await axios.patch(
+          `/api/workouts/${formData._id}`,
+          formData
+        );
+            if (response.status === 200) {
+              onNewWorkout(response.data);
+              setFormData({ title: "", load: "", reps: "" });
+              setEmptyFields([]);
+              setError();
+            } 
+      } catch (error) {
+        console.error("Error:", error);
       }
-    } catch (error) {
-      // Handle any network errors or errors thrown during the request
-      if (error.response && error.response.data) {
-        setError(error.response.data.error);
-        setEmptyFields(error.response.data.emptyFields);
-      } else {
-        setError("An unexpected error occurred");
+    } else {
+      try {
+        const response = await axios.post("/api/workouts", formData);
+        //   console.log("the res is ", response);
+        if (response.status === 200) {
+          onNewWorkout(response.data);
+          setFormData({ title: "", load: "", reps: "" });
+          setEmptyFields([]);
+          setError();
+        }
+      } catch (error) {
+        // Handle any network errors or errors thrown during the request
+        if (error.response && error.response.data) {
+          setError(error.response.data.error);
+          setEmptyFields(error.response.data.emptyFields);
+        } else {
+          setError("An unexpected error occurred");
+        }
+        console.error("Error:", error);
       }
-      console.error("Error:", error);
     }
+  };
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+      
+    }));
   };
 
   return (
     <div className="mt-10 p-2  ">
-      <div className=" p-10 rounded-lg bg-[#09150c]   ">
+      <div className=" p-8 rounded-lg bg-[#09150c]   ">
         <form action="" onSubmit={handleSubmit} className="text-center">
           <h3 className="text-xl font-medium text-myfontcolor text-center">
             {isEdit ? "Edit Workout" : "Add Workout"}
@@ -63,7 +87,8 @@ const WorkoutForm = ({ onNewWorkout, workout, isEdit }) => {
             value={formData.title}
             placeholder="Exercise title"
             type="text"
-            onChange={(e) => setTitle(e.target.value)}
+            name="title"
+            onChange={handleInputChange}
           />
 
           <br />
@@ -76,7 +101,8 @@ const WorkoutForm = ({ onNewWorkout, workout, isEdit }) => {
             placeholder="Load (in kg )"
             type="number"
             value={formData.load}
-            onChange={(e) => setLoad(e.target.value)}
+            name="load"
+            onChange={handleInputChange}
           />
           <br />
           <input
@@ -88,7 +114,8 @@ const WorkoutForm = ({ onNewWorkout, workout, isEdit }) => {
             placeholder="Reps"
             type="number"
             value={formData.reps}
-            onChange={(e) => setReps(e.target.value)}
+            name="reps"
+            onChange={handleInputChange}
           />
           <br />
           <div className="mt-4 text-center">
